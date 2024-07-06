e16a56af2041bbaa185126bf0729a31206877c43
IT21314742 <it21314742@my.sliit.lk>
2024-07-06 19:32:19 +0530

    Update
    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
