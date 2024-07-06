092f2c1891d14cd7607b785f48a5f228a50953c1
IT21314742 <it21314742@my.sliit.lk>
2024-07-06 19:31:37 +0530

    Update
    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
