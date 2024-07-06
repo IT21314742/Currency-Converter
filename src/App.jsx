108cbb236bd9d80f1330c81d3ae86314542e0f0c
IT21314742 <it21314742@my.sliit.lk>
2024-07-06 19:31:48 +0530

    Update
    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
