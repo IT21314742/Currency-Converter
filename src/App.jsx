5dfb98cc95b2d7e8b6b3b3349d2020855d61fa02
IT21314742 <it21314742@my.sliit.lk>
2024-07-07 22:07:12 +0530

    Update
    Update

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     <form className="converter-form">
       <div className='form-group'>
         <label className="form-label">Enter Amount</label>
-        <input type="number" className='form-input'/>
+        <input type="number" className="form-input" required/>
       </div>
     </form>
     </div>
