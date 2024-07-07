598c0f57da6727b262ea78cd4fb76c015071bd6d
IT21314742 <it21314742@my.sliit.lk>
2024-07-07 22:00:36 +0530

    Update
    Update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className='currency-converter'>
-      <h2 className='converter-title'></h2>
+      <h2 className='converter-title'>Currency Converter</h2>
     <form className="converter-form"></form>
     </div>
   )
