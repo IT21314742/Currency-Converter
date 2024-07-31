12f5239c9e069c77b98d8967db225407cf63f0e5
IT21314742 <it21314742@my.sliit.lk>
2024-07-31 23:13:21 +0530

    Update
    Update

--- src/components/CurrencySelect.jsx
+++ src/components/CurrencySelect.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const CurrencySelect = () => {
   return (
-    
+    <div className="currency-select">
+    <img src="https://flagsapi.com/LK/flat/64.png" alt="Flag" />
+
+    <select className="currency-dropdown">
+      <option value="USD" >USD</option>
+      <option value="LKR" selected>LKR</option>
+      <option value="NPR" >NPR</option>
+    </select>
+  </div>
   )
 }
 
