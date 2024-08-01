e1f83c2cf0bc22b8303b5d610a799588b9b049b2
IT21314742 <it21314742@my.sliit.lk>
2024-08-01 21:36:47 +0530

    Update file
    Update file

--- src/components/ConverterForm.jsx
+++ src/components/ConverterForm.jsx
@@ -23,12 +23,12 @@ const ConverterForm = () => {
       </div>
 
       <div className="swap-icon">
-        <svg width="16" viewBox="0 0 20 19" xmlns="http://www.w3.org/2000/svg">
+        {/* <svg width="16" viewBox="0 0 20 19" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M19.13 11.66H.22a.22.22 0 0 0-.22.22v1.62a.22.22 0 0 0 .22.22h16.45l-3.92 4.94a.22.22 0 0 0 .17.35h1.97c.13 0 .25-.06.33-.16l4.59-5.78a.9.9 0 0 0-.7-1.43zM19.78 5.29H3.34L7.26.35A.22.22 0 0 0 7.09 0H5.12a.22.22 0 0 0-.34.16L.19 5.94a.9.9 0 0 0 .68 1.4H19.78a.22.22 0 0 0 .22-.22V5.51a.22.22 0 0 0-.22-.22z"
             fill="#fff"
           />
-        </svg>
+        </svg> */}
       </div>
 
       <div className="form-section">
